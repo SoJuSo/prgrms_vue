69d3fb7ea7b27854593b606b7959ae28598e5765
SoJuSo <asdfg7123@gmail.com>
2023-11-23 16:49:32 +0900

    Modify template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <h1
-    v-if="msg"
-    class="message"
-    @click="log">
+  <h1 v-if="msg" class="message" @click="log">
     {{ msg }}
   </h1>
-  <div></div>
-  <div></div>
   <Hello />
 </template>
 
 <script>
-// import Hello from "./components/Hello.vue"; //이게 문제가 될 수 있다.
-import Hello from '~/components/Hello' // ?????????????
+import Hello from "~/components/Hello";
 export default {
   components: {
     Hello,
   },
   data() {
     return {
-      msg: 'Hello Webpack?..',
-    }
+      msg: "Hello Vue",
+    };
   },
-  methods: {
-    log() {
-      window.addEventListener()
-      document.querySelector()
-    },
-  },
-}
+};
 </script>
