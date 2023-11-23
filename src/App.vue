af6c026dea32be78373591dfe2993a60498f675a
SoJuSo <asdfg7123@gmail.com>
2023-11-23 15:43:45 +0900

    Add eslint rules

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1
+    v-if="msg"
+    class="message"
+    @click="log">
+    {{ msg }}
+  </h1>
+  <div></div>
+  <div></div>
   <Hello />
 </template>
 
 <script>
 // import Hello from "./components/Hello.vue"; //이게 문제가 될 수 있다.
-import Hello from "~/components/Hello"; // ?????????????
+import Hello from '~/components/Hello' // ?????????????
 export default {
   components: {
     Hello,
   },
   data() {
     return {
-      msg: "Hello Webpack?..",
-    };
+      msg: 'Hello Webpack?..',
+    }
   },
-};
+  methods: {
+    log() {
+      window.addEventListener()
+      document.querySelector()
+    },
+  },
+}
 </script>
