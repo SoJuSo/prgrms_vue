44a1b9f46ed9ab09b8638ae693eb9ccca532265c
SoJuSo <asdfg7123@gmail.com>
2023-11-25 02:11:08 +0900

    Day49_9_1

---
+++ src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Child! / {{ msg }}</h2>
+</template>
+
+<script>
+export default {
+  inject: ["msg"],
+  mounted() {
+    console.log(this.msg);
+  },
+  //   props: {
+  //     msg: {
+  //       type: String,
+  //       default: "",
+  //     },
+  //   },
+};
+</script>
---
+++ src/components/Parent.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Parent</h1>
+  <Child />
+</template>
+
+<script>
+import Child from "~/components/Child";
+export default {
+  components: {
+    Child,
+  },
+  // props: {
+  //   msg: {
+  //     type: String,
+  //     default: "",
+  //   },
+  // },
+};
+</script>
+
+<style scoped lang="scss">
+$color: red;
+h1 {
+  color: $color;
+}
+</style>
