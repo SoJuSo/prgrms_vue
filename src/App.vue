81304bbafc516ab50d2c253a3c5fd9c20ec8347c
SoJuSo <asdfg7123@gmail.com>
2023-11-24 18:21:56 +0900

    Day49_4_2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <h1>
+  <div v-if="!isEdit">
     {{ msg }}
-  </h1>
-  <Hello />
+    <button @click="onEdit">Edit!</button>
+  </div>
+  <div v-else>
+    <input ref="editor" v-model="msg" type="text" @keyup.enter="isEdit = false" />
+  </div>
 </template>
 
 <script>
-import Hello from "~/components/Hello";
 export default {
-  components: {
-    Hello,
-  },
   data() {
     return {
+      isEdit: false,
       msg: "Hello Vue!",
     };
+  },
+  methods: {
+    onEdit() {
+      this.isEdit = true;
+      // 바로 보려고 하니까 에러가 난다.
+      // setTimeout(() => {
+      this.$nextTick(() => {
+        this.$refs.editor.focus();
+      }, 0); //callstack을 통해서 이 호출은 뒤로 밀린다.
+    },
   },
 };
 </script>
