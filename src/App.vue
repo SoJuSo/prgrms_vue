f1434c9d4ae7b519f977560e95903f1a51c2215b
SoJuSo <asdfg7123@gmail.com>
2023-11-28 18:15:37 +0900

    Day51_4_1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <RouterLink to="/about">About..</RouterLink>
-  <RouterView />
+  <header>
+    <RouterLink to="/">Home..</RouterLink>
+    <RouterLink to="/about">About..</RouterLink>
+  </header>
+  <section>
+    <RouterView />
+  </section>
 </template>
 
+<style lang="scss" scoped>
+header {
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+}
+
+section {
+  margin-top: 80px;
+}
+</style>
+
 <script>
-export default {
-  data() {
-    return {
-      msg: "Hello Vue!",
-    };
-  },
-};
+export default {};
 </script>
--- src/routes/index.js
+++ src/routes/index.js
@@ -15,11 +15,25 @@ export default createRouter({
 
 // HTML 5 mode
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "./Home";
 import About from "./About";
 
 export default createRouter({
   history: createWebHistory(),
+  scrollBehavior(to, from, savedPosition) {
+    // return { top: 0 };
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ left: 0, top: 0 });
+      }, 500);
+    });
+  },
   routes: [
+    {
+      path: "/",
+      component: Home,
+    },
     {
       path: "/about",
       component: About,
