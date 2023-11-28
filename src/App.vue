6ed109020a090e9f609e2c0943a63d9f0d974778
SoJuSo <asdfg7123@gmail.com>
2023-11-28 12:50:29 +0900

    Day50_4_1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <h1>
-    {{ msg }}
-  </h1>
-  <Hello />
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterLink
+    :to="{
+      name: 'docsId',
+      params: { id: '7777' },
+      query: {
+        name: 'kim',
+        age: 26,
+        email: '123@123.com',
+      },
+    }"
+  >
+    Documents Id
+  </RouterLink>
+  <!-- <button @click="$router.push('/')">Home</button>
+  <button @click="$router.push('/about')">About</button> -->
+  <button @click="$router.push({ name: 'home' })">Home</button>
+  <button @click="$router.push({ name: 'about' })">About</button>
+  <RouterView />
 </template>
 
 <script>
-import Hello from "~/components/Hello";
 export default {
-  components: {
-    Hello,
-  },
   data() {
     return {
       msg: "Hello Vue!",
