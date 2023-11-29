4132ad4bde62efe69c8ed29bc927501f40b21ebe
SoJuSo <asdfg7123@gmail.com>
2023-11-29 12:52:53 +0900

    Day49_6_1

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
   <h1>
     {{ msg }}
   </h1>
+  <h1>
+    {{ count }}
+  </h1>
   <Hello />
 </template>
 
 <script>
 import Hello from "~/components/Hello";
+import sampleMixin from "~/mixins/sample";
 export default {
+  // mixins: [sampleMixin], // 컴포넌트보다 아래 작성해야 함
   components: {
     Hello,
   },
+  mixins: [sampleMixin],
   data() {
     return {
       msg: "Hello Vue!",
